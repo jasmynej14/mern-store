eddb1b4a8a0d46256dc486bef85015803b17fb59
jasmynej14 <jasmynej14@brandeis.edu>
2021-05-25 13:01:55 -0400

    connect mongo

--- store-back/server.js
+++ store-back/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import colors from 'colors';
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 app.get('/',(req,res)=> {
     res.send("nodemon testy test")
@@ -19,4 +23,4 @@ app.get('/projewels/products/:id',(req,res) => {
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT,console.log(`Server running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`))
+app.listen(PORT,console.log(`Server running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`.cyan.underline))
